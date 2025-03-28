dc05a85ec4c07c3d7c54f8a2f1c92ff7650d4ebc
Santosh <128799305+santosh-gs@users.noreply.github.com>
2025-03-28 23:49:40 +0530

    Update pythong_vending_machine.py
    Refactored logic to check conditions in a more natural order.

--- Certification Exams/Python Basic/pythong_vending_machine.py
+++ Certification Exams/Python Basic/pythong_vending_machine.py
@@ -5,9 +5,23 @@ method buy(req_items, money)
 2. if fewer items in machine, ValueError
 3. if not enough money for req_items, ValueError
 '''
-
 class VendingMachine:
-    
+    def __init__(self, num_items, item_price):
+        self.num_items = num_items
+        self.item_price = item_price
+
+    def buy(self, req_items, money):
+        if req_items > self.num_items:
+            raise ValueError("Not enough items in the machine")
+        
+        if money < req_items * self.item_price:
+            raise ValueError("Not enough money")
+        
+        self.num_items -= req_items
+        return money - req_items * self.item_price
+
+'''
+class VendingMachine:
     def __init__(self, num_items, item_price):
         self.num_items = num_items
         self.item_price = item_price
@@ -22,3 +36,4 @@ class VendingMachine:
         
         else:
             raise ValueError ("Not enough coins")
+'''

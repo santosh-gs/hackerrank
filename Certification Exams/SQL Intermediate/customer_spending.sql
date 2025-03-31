eeb2684be124cb10c330fd655b807ae40fad71e4
Santosh <128799305+santosh-gs@users.noreply.github.com>
2025-03-31 20:30:31 +0530

    Update customer_spending.sql

--- Certification Exams/SQL Intermediate/customer_spending.sql
+++ Certification Exams/SQL Intermediate/customer_spending.sql
@@ -11,3 +11,6 @@ ON customer.id = invoice.customer_id
 GROUP BY customer.customer_name 
 HAVING SUM(invoice.total_price) <= 0.25 * (SELECT AVG(invoice.total_price) FROM invoice) 
 ORDER BY SUM(invoice.total_price) DESC;
+
+-- there seem to be an issue earlier towards hackerrank end regarding answer validation
+-- also it isn't intuitive to calculate the SUM from the question as far as i remember
